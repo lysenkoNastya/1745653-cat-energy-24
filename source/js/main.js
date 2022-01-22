4eeb67b925e69341cf73aca43127fbfb66f25cec
anastasiia <asya16828@gmail.com>
2022-01-23 00:47:42 +0200

    Изменение стилей/main.js/nav--index

--- source/js/main.js
+++ source/js/main.js
@@ -2,5 +2,9 @@ const nav = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 const noJsWrap = document.querySelector('.no-js');
 
+if (noJsWrap) {
+  noJsWrap.classList.remove('no-js');
+}
+
+
 navToggle.addEventListener('click', () => nav.classList.toggle('nav--opened'));
-noJsWrap.classList.remove('no-js');
